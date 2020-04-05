2e8d3a0c5be6c4c5971acc67b4e274402f1924ac
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-04-05 17:48:43 -0400

    imported layout components and added base styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import { Grid } from '@material-ui/core';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 
-function App() {
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    main: {
+      height: '100vh',
+    },
+    items: {
+      padding: theme.spacing(4),
+    },
+  })
+);
+
+const App: React.FunctionComponent<{}> = () => {
+  const classes = useStyles();
   return (
     <React.Fragment>
       <CssBaseline />
       <div className="App"></div>
     </React.Fragment>
   );
-}
+};
 
 export default App;
