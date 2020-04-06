c535917f195d76ccbf3477b05bbd496dce25db3e
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-04-05 21:23:32 -0400

    added theme primary and secondary colours with types

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,52 @@
 import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { Grid } from '@material-ui/core';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import {
+  createMuiTheme,
+  createStyles,
+  makeStyles,
+  Theme as AugmentedTheme,
+  ThemeProvider,
+} from '@material-ui/core/styles';
 
-const useStyles = makeStyles((theme: Theme) =>
+declare module '@material-ui/core/styles/createMuiTheme' {
+  interface Theme {
+    Palette: {
+      primary: {
+        main: string;
+      };
+      secondary: {
+        secondary: {
+          main: string;
+        };
+      };
+    };
+  }
+
+  interface ThemeOptions {
+    Palette?: {
+      primary?: {
+        main?: string;
+      };
+      secondary?: {
+        main?: string;
+      };
+    };
+  }
+}
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#038C8C',
+    },
+    secondary: {
+      main: '#025E73',
+    },
+  },
+});
+
+const useStyles = makeStyles((theme: AugmentedTheme) =>
   createStyles({
     main: {
       height: '100vh',
@@ -18,13 +61,15 @@ const App: React.FunctionComponent<{}> = () => {
   const classes = useStyles();
   return (
     <React.Fragment>
-      <CssBaseline />
-      <div className="App">
-        <Grid container component="main" className={classes.main}>
-          <Grid item xs={12} md={4} className={classes.items}></Grid>
-          <Grid item xs={12} md={8} className={classes.items}></Grid>
-        </Grid>
-      </div>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="App">
+          <Grid container component="main" className={classes.main}>
+            <Grid item xs={12} md={4} className={classes.items}></Grid>
+            <Grid item xs={12} md={8} className={classes.items}></Grid>
+          </Grid>
+        </div>
+      </ThemeProvider>
     </React.Fragment>
   );
 };
