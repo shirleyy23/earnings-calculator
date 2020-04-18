4ad94ff525e5cd65d4cdc59125b72e3e1e1572c7
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-04-18 14:22:36 -0400

    Updated header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,18 +2,23 @@ import React from 'react';
 import { Typography } from '@material-ui/core';
 
 interface Props {
-	title: string;
+  title: string;
 }
 
-const Header: React.FunctionComponent<Props> = (props) => {
-	return (
-		<header>
-			<Typography component="h1" variant="h3" gutterBottom color="primary">
-				{props.title}
-			</Typography>
-      ;
-		</header>
-	);
+const Header: React.FunctionComponent<Props> = ({ title }) => {
+  return (
+    <header>
+      <Typography
+        data-testid="Header"
+        component="h1"
+        variant="h3"
+        gutterBottom
+        color="primary"
+      >
+        {title}
+      </Typography>
+    </header>
+  );
 };
 
 export default Header;
