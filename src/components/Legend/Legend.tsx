9380a52b7b6b04cf92578066c6a6d92911e6579c
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-04-19 19:39:05 -0400

    added styles and corresponding classes to icon and list item

--- src/components/Legend/Legend.tsx
+++ src/components/Legend/Legend.tsx
@@ -2,16 +2,38 @@ import React from 'react';
 
 import ListItem from '@material-ui/core/ListItem';
 
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+
 interface Props {
   title: string;
   color: string;
 }
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    listItem: {
+      display: 'inline-block',
+      marginRight: '1rem',
+      fontSize: 12,
+      color: '#666',
+      width: 'auto',
+    },
+    legendIcon: {
+      width: '10px',
+      height: '10px',
+      display: 'inline-block',
+      marginRight: '.25rem',
+      backgroundColor: (props: Props): string => props.color,
+    },
+  })
+);
+
 const Legend: React.FunctionComponent<Props> = (Props) => {
+  const classes = useStyles(Props);
   const { title } = Props;
   return (
-    <ListItem>
-      <span></span>
+    <ListItem className={classes.listItem}>
+      <span className={classes.legendIcon}></span>
       {title}
     </ListItem>
   );
